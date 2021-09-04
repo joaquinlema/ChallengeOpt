44945e704a6e8ab6195607c0c934c0ca99be5ac0
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-09-04 10:29:23 -0300

    agrgeamos hoc de form en app js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import HomeState from './context/homeContext/HomeState';
 import NavDrawer from './components/home/NavDrawer';
+import FormState from './context/formContext/FormState';
 
 function App() {
   return (
     <BrowserRouter>
       <HomeState>
-        <NavDrawer />
+        <FormState>
+          <NavDrawer />
+        </FormState>
       </HomeState>
     </BrowserRouter>
   );
